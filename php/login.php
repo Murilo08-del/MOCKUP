ffdc93ab2886599ecd1ae07829f4c706d62c4872
Murilo08-del <murilo_h_da-costa@estudante.sesisenai.org.br>
2025-10-10 19:07:24 -0300

    todos os caminhos corretos

--- php/login.php
+++ php/login.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 $_SESSION['nome'] = $dados['nome'];
 
                 $_SESSION["conectado"] = true;
-                header("location: dashboard.php ");
+                header("location: ../html/gestormonitoramento.html");
                 exit;
             } else {
                 $erro = "Usuário ou senha inválidos";
